3003beb0dd66819a5e07dfc32e19c05f403761c1
Devansh-react <devanshs748@gmail.com>
2025-02-09 01:43:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './components/Hero.js'
+import Navbar from './components/Navbar/Navbar.js'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
