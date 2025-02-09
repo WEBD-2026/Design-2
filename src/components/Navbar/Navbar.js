df4f930cd7a3250ff296429c472f3b6c26c80780
Devansh-react <devanshs748@gmail.com>
2025-02-09 15:58:00 +0530

    added buttons for the nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,60 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { FiMenu } from "react-icons/fi";
-import { GrClose } from "react-icons/gr";
-import "./Navbar.css"; // Import the CSS file
+import React from 'react';
+import styled from 'styled-components';
+import './Navbar.css'
 
-const Navbar = () => {
-    const [openMenu, setOpenMenu] = useState(false);
-    const [scrolling, setScrolling] = useState(false);
-
-    // Toggle Mobile Menu
-    const menuHandler = () => {
-        setOpenMenu(!openMenu);
-    };
-
-    // Scroll Effect
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolling(true);
-            } else {
-                setScrolling(false);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
+const Button = () => {
     return (
-        <>
-            <nav className={`navbar ${scrolling ? "scrolling" : ""}`}>
-                {/* Desktop Navigation */}
-                <ul className="nav-links">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Pricing</a></li>
-                    <li><a href="/">Testimonials</a></li>
-                </ul>
 
-                {/* Mobile Menu Button */}
-                <div className="menu-btn" onClick={menuHandler} aria-label="Menu">
-                    {openMenu ? <GrClose size={25} /> : <FiMenu size={25} />}
-                </div>
+        <div className='navbar'>
+            <button className="button">
+                <div><span>EVENT</span></div>
+            </button>
+            <button className="button">
+                <div><span>COMPETATIONS</span></div>
+            </button>
+            <button className="button">
+                <div><span>ACCOMODATION</span></div>
+            </button>
+            <button className="button">
+                <div><span>SCHEDULE</span></div>
+            </button>
+            <button className="button">
+                <div><span>CONTACT  </span></div>
+            </button>
+            <button className="button">
+                <div><span>REGISTER</span></div>
+            </button>
 
-                {/* Mobile Navigation */}
-                <ul className={`nav-links-mobile ${openMenu ? "open" : ""}`}>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Pricing</a></li>
-                    <li><a href="/">Testimonials</a></li>
-                </ul>
-            </nav>
+        </div>
 
-            {/* Backdrop for mobile menu */}
-            {openMenu && <div className="menu-backdrop show" onClick={menuHandler}></div>}
-        </>
+
     );
-};
+}
 
-export default Navbar;
+
+export default Button;
