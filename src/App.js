4fc4b4cd441668017251b17f97c16e4b6f8d0e06
Devansh-react <devanshs748@gmail.com>
2025-02-13 22:55:47 +0530

    added accomodation section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './index.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Event from './pages/Events/Event.jsx'
+import Accommodation from './pages/Accomodation/accomodation.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path={'/'} element={<Home />} />
       <Route path={'/event'} element={<Event />} />
+      <Route path={'/Accommodation'} element={<Accommodation />} />
+
     </Routes>
   )
 }
--- src/pages/Accomodation/accomodation.jsx
+++ src/pages/Accomodation/accomodation.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
-function Accomodation() {
+const accomodation = () => {
     return (
-        <divv class={}>Accomodation</div>
+        <div>
+            <div className="brutalist-card">
+                <div className="brutalist-card__header">
+                    <img src={require("./Background/Accomodation_background.webp")} alt="Event" />
+                </div>
+
+                <div className="brutalist-card__actions">
+                    <div className="brutalist-card_mess">
+                    </div>
+
+
+                    <div className="brutalist-card_butt">
+                        <button className="btn-class-name ">
+                            <button>
+                                Register
+                            </button>
+                        </button>
+                        <button className="btn-class-name">
+                            <button>
+                                More Info
+                            </button>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Accomodation
+export default accomodation
