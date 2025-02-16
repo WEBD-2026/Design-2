dabf2e5f2350e08c28c16c1962e2017f1de24a0c
Devansh-react <devanshs748@gmail.com>
2025-02-16 23:16:04 +0530

    commit-qwer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home/Home.jsx";
 import Event from "./pages/Events/Event.jsx";
 import Accommodation from "./pages/Accomodation/accomodation.jsx";
 import Schedule from "./pages/Schedule/Schedule.jsx";
+import Footer from "./components/Footer/Footer.js"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path={"/event"} element={<Event />} />
       <Route path={"/Accomodation"} element={<Accommodation />} />
       <Route path={"/schedule"} element={<Schedule />} />
+      <Route path={"/Footer"} element={<Footer />} />
     </Routes>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import image1 from "../../images/background.png";
+import { useEffect } from "react";
 
 const Button = () => {
   const [clicked, setClicked] = useState(false);
@@ -73,5 +74,68 @@ const Button = () => {
     </>
   );
 };
+// Add functionality to hide navbar on scroll up and show on scroll down
+
+// const Button = () => {
+//   const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+//   const [visible, setVisible] = useState(true);
+
+//   const handleScroll = () => {
+//     const currentScrollPos = window.pageYOffset;
+//     setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+//     setPrevScrollPos(currentScrollPos);
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, [prevScrollPos, visible, handleScroll]);
+
+//   return (
+//     <div className={`navbar ${visible ? "visible" : "hidden"}`}>
+//       <Link to={"/"}>
+//         <button className="button">
+//           <div>
+//             <span>HOME</span>
+//           </div>
+//         </button>
+//       </Link>
+
+//       <Link to={"/event"}>
+//         <button className="button">
+//           <div>
+//             <span>EVENT</span>
+//           </div>
+//         </button>
+//       </Link>
+//       <Link to={"/Accomodation"}>
+//         <button className="button">
+//           <div>
+//             <span>ACCOMODATION</span>
+//           </div>
+//         </button>
+//       </Link>
+
+//       <Link to={"/schedule"}>
+//         <button className="button">
+//           <div>
+//             <span>SCHEDULE</span>
+//           </div>
+//         </button>
+//       </Link>
+
+//       <button className="button">
+//         <div>
+//           <span>CONTACT </span>
+//         </div>
+//       </button>
+//       <button className="button">
+//         <div>
+//           <span>REGISTER</span>
+//         </div>
+//       </button>
+//     </div>
+//   );
+// };
 
 export default Button;
