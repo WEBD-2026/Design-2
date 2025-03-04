60f6d1a85d98bea8abfabd1e4b84e54a6a06de53
Shubham <98712331+skgarg18@users.noreply.github.com>
2025-03-05 04:16:58 +0530

    Events Changes

--- src/textBlock.js
+++ src/textBlock.js
@@ -14,7 +14,7 @@ function TextBlock() {
             <br />
             <br />
           </div>
-          <p id="textblock-content">
+          <p id="textblock-content" class="text-justify">
             Utkansh-25 at NIT Jalandhar blends culture and technology in perfect harmony. Experience electrifying performances like Panache, Megasonic, BDM Night, and Star Night, alongside innovative events like Hackathons, RC Racing, NIT-yaan, Startup Mela, and Avishkar Exhibitions. Enjoy ISRO sessions, E-Gaming, and more, culminating in a breathtaking Drone Show. This is Utkansh-where brilliance knows no boundaries, and the future takes center stage. Join us for an unforgettable celebration of talent, innovation, and creativity that promises to inspire and entertain.
             <br />
             <br />
