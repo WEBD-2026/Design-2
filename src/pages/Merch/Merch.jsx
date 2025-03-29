0d5a5eb8d2985d99da9cb63692d2af95f8b1fdec
Devansh-react <devanshs748@gmail.com>
2025-03-29 13:46:39 +0530

    add merch section

--- src/pages/Merch/Merch.jsx
+++ src/pages/Merch/Merch.jsx
@@ -1,72 +1,66 @@
 import React from 'react';
-import styled from 'styled-components';
+import Navbar from "../../components/Navbar/Navbar.js";
+import "./merct.css";
 
 const Merch = () => {
-    return (
-        <StyledWrapper>
-            <div className="card">
-                <div className="card-inner">
-                    <div className="card-front">
-                        <p>Front Side</p>
-                    </div>
-                    <div className="card-back">
-                        <p>Back Side</p>
-                    </div>
-                </div>
+  return (
+    <div className="w-screen min-h-screen pt-24 bg-left-bottom bg-no-repeat"
+      style={{
+        backgroundImage: "linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url('bg.png')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}>
+      <Navbar />
+      <div className="flex flex-col items-center px-2">
+        <div className='merch-head'>
+          <h1 className="text-white text-5xl md:text-6xl lg:text-8xl text-center"
+            style={{ fontFamily: "Rockybilly", WebkitTextStroke: "1px #ffffff", fontSize: "4vw", marginTop: "5vw" }}>
+            Merchandise
+          </h1>
+          <a href="https://forms.gle/vU1mspH4nRW2pU5h6" className="button-merch">
+            Buy Now
+            <svg className="icon" viewBox="0 0 24 24" fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a>
+        </div>
+
+        {/* ✅ Fix: Add a flex container to align cards side by side */}
+        <div className="card-container">
+          {/* T-Shirt 1 */}
+          <div className="card">
+            <div className="card-inner">
+              <div className="card-front">
+                <img src="3.png" alt="T-shirt front" className='mercht' />
+              </div>
+              <div className="card-back">
+                <img src="4.png" alt="T-shirt back" className='mercht' />
+              </div>
             </div>
-        </StyledWrapper>
-    );
+          </div>
+          {/* T-Shirt 2 */}
+          <div className="card">
+            <div className="card-inner">
+              <div className="card-front">
+                <img src="6.png" alt="T-shirt front" className='mercht' />
+              </div>
+              <div className="card-back">
+                <img src="5.png" alt="T-shirt back" className='mercht' />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+
+        </div>
+
+      </div>
+    </div>
+  );
 }
-
-const StyledWrapper = styled.div`
-  .card {
-    width: 300px;
-    height: 200px;
-    perspective: 1000px;
-  }
-
-  .card-inner {
-    width: 100%;
-    height: 100%;
-    position: relative;
-    transform-style: preserve-3d;
-    transition: transform 0.999s;
-  }
-
-  .card:hover .card-inner {
-    transform: rotateY(180deg);
-  }
-
-  .card-front,
-  .card-back {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    backface-visibility: hidden;
-  }
-
-  .card-front {
-    background-color: #6A2C70;
-    color: #fff;
-    display: flex;
-    align-items: center;
-    border: 10px solid #6A2C70;
-    border-radius: 10px;
-    justify-content: center;
-    font-size: 24px;
-    transform: rotateY(0deg);
-  }
-
-  .card-back {
-    background-color: #F08A5D;
-    color: #fff;
-    display: flex;
-    align-items: center;
-    border: 10px solid #F08A5D;
-    border-radius: 10px;
-    justify-content: center;
-    font-size: 24px;
-    transform: rotateY(180deg);
-  }`;
 
 export default Merch;
